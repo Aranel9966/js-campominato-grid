be1e39f6f2c71321ae7337b5b030ff7814f3fe1b
Aranel9966 <99angelo.dire@gmail.com>
2023-03-03 15:28:49 +0100

    scaffolding and add function createSquare

---
+++ js/script.js
@@ -0,0 +1,77 @@
+/*
+Consegna
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+*/
+
+/*
+
+->>select per le difficolta 
+?difficolta 1  => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+?:difficolta 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+?:difficolta 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+-->generare una griglia al clicc
+    ->ogni cella ha un numero progressivo (da 1 a 100)
+        ->10 righe 10 casselle 
+->al clicca di una cassella cambia colore in azzurro 
+->emette un messaggio in consol del numero
+
+
+*/
+
+let difficultyEL = document.getElementById('difficulty');
+let startBtnEl = document.getElementById('start-btn');
+let containerSquareEl = document.getElementById('container-square');
+
+
+
+startBtnEl.addEventListener('click',function(){
+    
+
+    if(difficultyEL.value == 1){
+        for(let i = 1; i <= 100;i++){
+
+            createSquare(containerSquareEl)
+
+        }
+
+    }else if(difficultyEL.value == 2){
+        for(let i = 1; i <= 81;i++){
+
+            createSquare(containerSquareEl)
+
+        }
+
+    }else{
+        for(let i = 1; i <= 49 ;i++){
+
+            createSquare(containerSquareEl)
+
+        }
+
+    }
+
+    // console.log(difficultyEL.value)
+})
+
+
+
+
+
+
+//////////function/////////
+
+function createSquare(container){
+    let newSquare = document.createElement('div')
+    newSquare.classList.add('square')
+    container.append(newSquare)
+    return newSquare
+}
